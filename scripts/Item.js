c9cfdbae25a0317f5d8f2acf1615d4eb5f5b8e2d
Terrance Corley <terrancecorley@outlook.com>
2018-04-09 13:16:41 -0700

    Adding IIFE to item.js

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,23 @@
 'use strict';
-const Item = (function(){  
-  return {
+/* global cuid */
+
+const Item = (function(){ 
+	const validateName = function(name) {
+		if (name.length === 0) {
+			throw new Error('Name does not exist.');
+		}
+	}
+	
+	const create = function(name) {
+		return {
+			id: cuid(),
+			name,
+			checked: false,
+		};
+	}
+
+	return {
+		validateName,
+		create,
   };
 }());
